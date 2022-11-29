21df37c042488b9a1e864407a7b5d12f9e67760e
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-11-28 22:48:17 -0300

    TP04 Completed

---
+++ TP4Java/Cargo.java
@@ -0,0 +1,29 @@
+/**
+ * @author Rodrigo Rebelo e Luiz Gustavo
+ */
+
+package tp04;
+
+public class Cargo
+{
+    private int codigo;
+    private String cargo;
+   
+    public Cargo(int codigo, String cargo){
+        this.codigo = codigo;
+        this.cargo = cargo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public int getCodigo(){
+        return this.codigo;
+    }
+    public String getCargo(){
+        return this.cargo;
+    }
+}
